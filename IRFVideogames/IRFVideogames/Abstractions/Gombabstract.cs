edd1328aad29e9d53eac3473ccea099fc644bdbf
Sakos12 <solymossy.akos@gmail.com>
2020-12-12 22:54:18 +0100

    Absztrakt osztály a gomboknak újracsinálva

---
+++ IRFVideogames/IRFVideogames/Abstractions/Gombabstract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRFVideogames.Abstractions
+{
+    public abstract class Gombabstract : Button
+    {
+            public Gombabstract()
+            {
+                AutoSize = false;
+                Width = 100;
+                Height = 100;
+            }
+            private void Abstract_Paint(object sender, PaintEventArgs e)
+            {
+                DrawImage(e.Graphics);
+            }
+
+            protected abstract void DrawImage(Graphics g);
+        }
+    }
+}
