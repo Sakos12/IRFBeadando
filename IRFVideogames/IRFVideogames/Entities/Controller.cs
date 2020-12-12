41cac9f2bcae158cf8b1faa162251d1cde5061ab
Sakos12 <solymossy.akos@gmail.com>
2020-12-12 22:57:44 +0100

    Controller osztály elkészítése

---
+++ IRFVideogames/IRFVideogames/Entities/Controller.cs
@@ -0,0 +1,19 @@
+﻿using IRFVideogames.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRFVideogames.Entities
+{
+    public class Controller : Gombabstract
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/playstation.jpg");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
